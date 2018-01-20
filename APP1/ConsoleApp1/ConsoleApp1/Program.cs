a56fb1a66691829b09330df4b5b3301d9ae9438b
zhouLJ <1661513776@qq.com>
2018-01-20 16:03:22 +0800

    C#  学习例程

---
+++ APP1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序*/
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
+
